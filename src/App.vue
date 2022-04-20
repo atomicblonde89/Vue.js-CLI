0b5a35ddccf593f8af19bf6d06b16ade734a9f9d
Demian <learc47@gmail.com>
2022-04-20 01:13:14 -0400

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,62 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app" class="container">
+        <h1 class="text-center">Tabla de Usuarios</h1>
+        <table class="table table-bordered table-hover table-striped">
+            <thead>
+                <tr>
+                    <th>#</th>
+                    <th>Nombre</th>
+                    <th>Apellido</th>
+                    <th>Run</th>
+                    <th>Fecha Nacimiento</th>
+                    <th>Edad</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="(usuario,i) in usuarios" :key="i">
+                    <th>{{i+1}}</th>
+                    <td>{{usuario.nombre}}</td>
+                    <td>{{usuario.apellido}}</td>
+                    <td>{{usuario.run}}</td>
+                    <td>{{usuario.nacimiento}}</td>
+                    <td>{{usuario.edad}}</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+ name: "App",
+ data: () => {
+               let result = { usuarios: [
+                    {
+                        nombre: 'Jose',
+                        apellido: 'Pérez',
+                        run: '123456-2',
+                        nacimiento: '12-04-67',
+                        edad: 52,},
+                    {
+                        nombre: 'María',
+                        apellido: 'Rodríguez',
+                        run: '654321-1',
+                        nacimiento: '30-11-90',
+                        edad: 29,},
+                    
+                    {   nombre: 'Manuel',
+                        apellido: 'Patiño',
+                        run: '142223456-6',
+                        nacimiento: '02-12-59',
+                        edad: 60,},
+                ]
+            }
+            return result
+        }
+        }
+        
+        
 </script>
 
 <style>
